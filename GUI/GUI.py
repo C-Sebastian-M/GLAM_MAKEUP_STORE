03656327b221f591228b387c7fa2d7ccf5696dd5
Sebastian Cortes Mejia <sebastiancortes303190@correo.itm.edu.co>
2024-05-14 21:09:04 -0500

    The login window was created according to the design and a background for the windows was added to the images folder

--- GUI/GUI.py
+++ GUI/GUI.py
@@ -1,6 +1,48 @@
-from tkinter import *
+import tkinter as tk
+import customtkinter
 
-root = Tk()
-root.geometry("800x600")
 
-root.mainloop()
+def open_registration_window():
+    registration_window = customtkinter.CTk()
+    registration_window.geometry("400x300")
+    registration_window.title("Registro")
+
+    # Agregar contenido a la ventana de registro, como etiquetas, campos de entrada, etc.
+    # Por ejemplo:
+    label = customtkinter.CTkLabel(
+        master=registration_window, text="Formulario de Registro", font=("Arial", 18)
+    )
+    label.pack(pady=20)
+
+    # Agregar más widgets aquí, como campos de entrada, botones, etc.
+
+    registration_window.mainloop()
+
+
+def open_income_window():
+    income_window = customtkinter.CTk()
+    income_window.geometry("400x300")
+    income_window.title("Ingresar")
+
+    label = customtkinter.CTkLabel(
+        master=income_window, text="Formulario de Ingreso", font=("Arial", 18)
+    )
+    label.pack(pady=20)
+    income_window.mainloop()
+
+
+customtkinter.set_appearance_mode("System")
+customtkinter.set_default_color_theme("green")
+
+app = customtkinter.CTk()
+app.geometry("600x440")
+app.title("Login")
+
+# Crear botón de registro
+register_button = customtkinter.CTkButton(
+    master=app, text="REGISTRARSE", command=open_registration_window
+)
+register_button.pack(pady=20)
+income_button = customtkinter.CTkButton(master=app,text="INGRESE", command=open_income_window)
+income_button.pack(pady = 50)
+app.mainloop()
