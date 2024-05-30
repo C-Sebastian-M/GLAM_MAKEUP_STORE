fb19b81b463f81ab1ffb9aefea7e142e3ef77d0e
JuanMedi <juanjosemedinasepulveda@gmail.com>
2024-05-30 15:11:34 -0500

    m

--- GUI/ventanas/Caja.py
+++ GUI/ventanas/Caja.py
@@ -1,11 +1,11 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtGui import QPainter, QBrush, QColor
 from PyQt5.QtCore import Qt
-
+#from sub_ventanas.GestionClientes import CBackground
 from PyQt5.QtWidgets import QApplication, QMainWindow, QMessageBox
 
 
-class Ui_Caja(object):  # terminada (falta reporte diario)
+class Ui_Caja(object, ):  # terminada (falta reporte diario)
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
         MainWindow.resize(800, 600)
@@ -122,7 +122,8 @@ class Ui_Caja(object):  # terminada (falta reporte diario)
         self.pushButtonC_2.setText(_translate("MainWindow", "Salir"))
         
     def report(self):#crear el metodo reporte diario
-        pass  
+        pass
+    
     def nextWindow(self):
         self.sele = QMainWindow()
         self.ui_backC = Sele_Compra()
