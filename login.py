ae9a42bbf22168b9aff3416ddf211d8fd033b8c8
DEVdanielp <danivive16@gmail.com>
2024-06-01 16:02:41 -0500

    Actualizacion del borrado y actualizacion de usuarios

--- login.py
+++ login.py
@@ -4,6 +4,7 @@ from GUI.ventanas.Caja_X import Ui_Caja
 from GUI.ventanas.soporte_admin import AdminSoporteManager
 from API.prueba import Cajero
 from DATA import GestionDatos
+from API.Validaciones import *
 
 
 class Login(QMainWindow):
@@ -13,6 +14,7 @@ class Login(QMainWindow):
             r"GUI\ui\Login.ui",
             self,
         )
+        self.gestion_datos = GestionDatos()
         self.app = app
         # BT CLOSE POPUP
         self.pushButton_close_pupup.clicked.connect(lambda: self.frame_error.hide())
@@ -65,30 +67,13 @@ class Login(QMainWindow):
             text = textUser + textPassword
             showMessage(text)
         else:
-            # Se implemento el login conectado a la base de datos
-            posi = 0
-            if (
-                username in self.cajero.gestion_datos.usuarios["usuario"].values
-                and password in self.cajero.gestion_datos.usuarios["contraseña"].values
-            ):
-                for index, row in self.cajero.gestion_datos.usuarios.iterrows():
-                    if row["usuario"] == username:
-                        break
-                posi = index
-            for index, row in self.cajero.gestion_datos.usuarios.iterrows():
-                if index == posi:
-                    user_role = row["Rol ID"]
-            if user_role == 1:
-                user_role = "soporte"
-            elif user_role == 2:
-                user_role = "admin"
-            elif user_role == 3:
-                user_role = "caja"
-
+            user_role = self.authenticate_user(username, password)
             if user_role:
-                if user_role == "admin" or user_role == "soporte":
-                    self.openAdminSupportWindow(user_role)
-                elif user_role == "caja":
+                if user_role == 1:
+                    self.openAdminSupportWindow("soporte")
+                elif user_role == 2:
+                    self.openAdminSupportWindow("admin")
+                elif user_role == 3:
                     self.openCajaWindow()
                 else:
                     showMessage("Credenciales incorrectas")
@@ -104,16 +89,13 @@ class Login(QMainWindow):
         msg_box.exec_()
 
     def authenticate_user(self, username, password):
-        # Aquí deberías tener la lógica de autenticación, por ejemplo, verificar las credenciales en una base de datos
-        # if username == "admin" and password == "admin":
-        #   return "admin"
-        # elif username == "caja" and password == "caja":
-        #   return "caja"
-        # elif username == "soporte" and password == "soporte":
-        #   return "soporte"
-        # else:
-        if self.cajero.login(username, password) == True:
-            return "admin"
+        usuario_datos = self.gestion_datos.usuarios[self.gestion_datos.usuarios["usuario"] == username]
+        if not usuario_datos.empty:
+            if password in usuario_datos["contraseña"].values:
+                rol = usuario_datos["Rol ID"].values
+                return int(rol)
+            return False
+        return False
 
     def openAdminSupportWindow(self, user_role: str):
         self.admin_soporte = AdminSoporteManager(self, user_role=user_role)
