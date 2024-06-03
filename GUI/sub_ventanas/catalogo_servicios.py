fcbcbab9a1e5ba9f4ed5535e79cb10c01fb5f1a4
Sebastian <sebastiancortes303190@correo.itm.edu.co>
2024-06-03 02:10:17 -0500

    gestion de servicios terminado, falta conectar con la base de datos

--- GUI/sub_ventanas/catalogo_servicios.py
+++ GUI/sub_ventanas/catalogo_servicios.py
@@ -1,10 +1,19 @@
 from PyQt5.uic import loadUi
-from PyQt5.QtWidgets import QMainWindow, QHeaderView, QTableWidgetItem, QMessageBox,QApplication
-from PyQt5.QtCore import QPropertyAnimation, Qt
+import pandas as pd
+from PyQt5.QtWidgets import (
+    QMainWindow,
+    QHeaderView,
+    QTableWidgetItem,
+    QMessageBox,
+    QCompleter,
+)
+from PyQt5.QtCore import QPropertyAnimation, Qt, QStringListModel
 from PyQt5 import QtCore, QtWidgets, QtGui
+from API.DATA import GestionDatos
 from PyQt5.QtGui import QPainter, QBrush, QColor
 from API.DATA import GestionDatos
-import sys
+from API.Validaciones import *
+
 
 class CBackground:
     def paintEvent(self, event):
@@ -47,13 +56,15 @@ class GestionServicios(QMainWindow, CBackground):
             self,
         )
         self.gestion_datos = GestionDatos()
-
         self.pushButton_menu.clicked.connect(self.mover_menu)
         # Botones
         self.pushButton_actualizar.clicked.connect(self.mostrar_servicios)
+        self.pushButton_mostrarModificar.clicked.connect(self.mostrar_servicioModificar)
+        self.pushButton_mostrarEliminar.clicked.connect(self.mostrar_servicioEliminar)
         # self.pushButton_add.clicked.connect(self.registrar_servicio)
-        # self.pushButton_guardarInfo.clicked.connect(self.modificar_servicio)
-        # self.pushButton_eliminar.clicked.connect(self.eliminar_servicio)
+        self.pushButton_guardarInfo.clicked.connect(self.modificar_servicio)
+        self.pushButton_eliminar.clicked.connect(self.eliminar_servicio)
+        self.pushButton_modificar.clicked.connect(self.mostar_formulario)
 
         self.gripSize = 10
         self.grip = QtWidgets.QSizeGrip(self)
@@ -84,41 +95,57 @@ class GestionServicios(QMainWindow, CBackground):
         self.tableWidget_eliminarServicio.horizontalHeader().setSectionResizeMode(
             QHeaderView.Stretch
         )
-        # self.setupValidatorsId()
-        # self.setupValidatorsPrecio()
-
-        # Ancho columna adaptable
+        self.tableWidget_modificar.horizontalHeader().setSectionResizeMode(
+            QHeaderView.Stretch
+        )
+        self.setupValidatorsId()
+        self.setupValidatorsPrecio()
+        self.frame_formulario.hide()
+        df = pd.read_excel("registros.xlsx", sheet_name="Servicios")
+        servicios = df["ID servicio"].astype(str).tolist()
+        self.modelo_datos = QStringListModel(servicios)
+        self.completer = QCompleter(self.modelo_datos, self)
+        self.completer.setCaseSensitivity(False)  # Ignorar mayúsculas y minúsculas
+        self.completer.setFilterMode(
+            Qt.MatchContains
+        )  # Coincidir con cualquier parte del texto
+        self.completer.setCompletionMode(QCompleter.PopupCompletion)
+        self.completer.setPopup(self.listView_buscar)
+        self.completer.setPopup(self.listView_eliminar)
+        self.lineEdit_modificar.setCompleter(self.completer)
+        self.lineEdit_buscarEliminar.setCompleter(self.completer)
 
     def resizeEvent(self, event):
         rect = self.rect()
         self.grip.move(rect.right() - self.gripSize, rect.bottom() - self.gripSize)
 
-    # Permite ingrear solo numeros a los campos y el paraletro del metodo es la cantidad de caracteres
-    
-    #def setupValidatorsId(self):
-    #    validacion_numero = QtGui.QRegularExpressionValidator(
-    #        QtCore.QRegularExpression(r"\d{12}")
-    #    )
-    #    self.lineEdit_addId.setValidator(validacion_numero)
-    #    self.lineEdit_nuevoId.setValidator(validacion_numero)
-    #    self.lineEdit_buscarModificar.setValidator(validacion_numero)
-    #    self.lineEdit_buscarEliminar.setValidator(validacion_numero)
-    
-    #def setupValidatorsPrecio(self):
-    #    validacion_numero = QtGui.QRegularExpressionValidator(
-    #        QtCore.QRegularExpression(r"\d{16}")
-    #    )
-    #    self.lineEdit_addPrecio.setValidator(validacion_numero)
-    #    self.lineEdit_nuevoPrecio.setValidator(validacion_numero)
-    #    self.lineEdit_buscarModificar.setValidator(validacion_numero)
-    #    self.lineEdit_buscarEliminar.setValidator(validacion_numero)
+    def setupValidatorsId(self):
+        validacion_numero = QtGui.QRegularExpressionValidator(
+            QtCore.QRegularExpression(r"\d{12}")
+        )
+        self.lineEdit_idServicio.setValidator(validacion_numero)
+        self.lineEdit_nuevoId.setValidator(validacion_numero)
+        self.lineEdit_modificar.setValidator(validacion_numero)
+        self.lineEdit_buscarEliminar.setValidator(validacion_numero)
+
+    def setupValidatorsPrecio(self):
+        validacion_numero = QtGui.QRegularExpressionValidator(
+            QtCore.QRegularExpression(r"\d{16}")
+        )
+        self.lineEdit_addPrecio.setValidator(validacion_numero)
+        self.lineEdit_nuevoPrecio.setValidator(validacion_numero)
+        self.lineEdit_modificar.setValidator(validacion_numero)
+        self.lineEdit_buscarEliminar.setValidator(validacion_numero)
 
     def limpiar_campos(self):
+        self.lineEdit_nuevoId.clear()
+        self.lineEdit_idServicio.clear()
         self.lineEdit_addPrecio.clear()
         self.lineEdit_addServicio.clear()
         self.lineEdit_buscarEliminar.clear()
         self.lineEdit_nuevoServicio.clear()
         self.lineEdit_nuevoPrecio.clear()
+        self.lineEdit_modificar.clear()
 
     def mover_menu(self):
         if True:
@@ -145,23 +172,19 @@ class GestionServicios(QMainWindow, CBackground):
             for j, (colname, value) in enumerate(row.items()):
                 self.tabla_verServicios.setItem(i, j, QTableWidgetItem(str(value)))
 
-    #def registrar_servicio(self):
-    #    id_servicio = self.lineEdit_addId.text()
-    #    nombre = self.lineEdit_addNombre.text()
-    #    precio = self.lineEdit_addPrecio.text()
-    #    if (
-    #        validar_Id(id_servicio)
-    #        and validar_Precio(precio)
-    #        and validar_NombreCom(nombre)
-    #    ):
-    #        self.gestion_datos.agregar_servicio(id_servicio, nombre, precio)
-    #        self.mostrar_servicios()
-    #        self.show_success_dialog("Servicio registrado con éxito.")
-    #        self.limpiar_campos()
-    #    else:
-    #        self.showErrorMessage(
-    #            "Error en los datos ingresados. Por favor, verifica la información."
-    #        )
+    def mostrar_servicioModificar(self):
+        self.tableWidget_modificar.setRowCount(0)
+        for i, row in self.gestion_datos.servicios.iterrows():
+            self.tableWidget_modificar.insertRow(i)
+            for j, (colname, value) in enumerate(row.items()):
+                self.tableWidget_modificar.setItem(i, j, QTableWidgetItem(str(value)))
+
+    def mostrar_servicioEliminar(self):
+        self.tableWidget_eliminarServicio.setRowCount(0)
+        for i, row in self.gestion_datos.servicios.iterrows():
+            self.tableWidget_Eliminar.insertRow(i)
+            for j, (colname, value) in enumerate(row.items()):
+                self.tableWidget_Eliminar.setItem(i, j, QTableWidgetItem(str(value)))
 
     def showErrorMessage(self, message):
         msg_box = QMessageBox()
@@ -177,6 +200,51 @@ class GestionServicios(QMainWindow, CBackground):
         msg_box.setWindowTitle("Éxito")
         msg_box.exec_()
 
+    # def registrar_servicio(self):
+    #    id_servicio = self.lineEdit_addId.text()
+    #   nombre = self.lineEdit_addNombre.text()
+    #    precio = self.lineEdit_addPrecio.text()
+    #    if (
+    #        validar_Id(id_servicio)
+    #        and validar_Precio(precio)
+    #        and validar_NombreCom(nombre)
+    #    ):
+    #        self.gestion_datos.agregar_servicio(id_servicio, nombre, precio)
+    #        self.mostrar_servicios()
+    #        self.label_aviso.setText("Servicio registrado con éxito")
+    #        self.show_success_dialog("Servicio registrado con éxito.")
+    #        self.limpiar_campos()
+    #    else:
+    #        self.showErrorMessage(
+    #            "Error en los datos ingresados. Por favor, verifica la información."
+    #        )
+
+    def validar_existencia(self):
+        # Los campos para validar son:
+        if self.lineEdit_modificar.text():
+            idBuscarServicio = self.lineEdit_modificar.text()
+            if (
+                idBuscarServicio in self.gestion_datos.servicios["ID servicio"].values
+                or int(idBuscarServicio) in self.gestion_datos.servicios["ID servicio"]
+            ):
+                return True
+            else:
+                return False
+        else:
+            self.aviso_modificar.setText("Campo Vacio. Por favor ingrese la información correspondiente.")
+        
+    def mostar_formulario(self, servicio):
+        servicio = self.validar_existencia()
+        if servicio:
+            self.frame_formulario.show()
+        else:
+            self.showErrorMessage(
+                "Error en los datos ingresados. Por favor, verifica la información."
+            )
+            self.aviso_modificar.setText(
+                "Error en los datos ingresados. Por favor, verifica la información."
+            )
+
     def modificar_servicio(self):
         id_servicio = self.lineEdit_.text()
         nuevos_datos = {
@@ -189,11 +257,4 @@ class GestionServicios(QMainWindow, CBackground):
     def eliminar_servicio(self):
         id_servicio = self.lineEdit_buscarEliminar.text()
         self.gestion_datos.eliminar_servicio(id_servicio)
-        self.mostrar_servicios()  # Actualizar la tabla de clientes"""
-
-
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    add = GestionServicios()
-    add.show()  # Asegúrate de mostrar la ventana
-    sys.exit(app.exec_())
+        self.mostrar_servicios()  # Actualizar la tabla de clientes
