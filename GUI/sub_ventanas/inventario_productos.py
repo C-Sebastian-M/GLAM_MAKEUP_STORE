e31fdba29185738bc657702afa7700b19442dbbe
Jhonatan-prog <110551929+Jhonatan-prog@users.noreply.github.com>
2024-06-03 22:26:51 -0500

    avanze en la venata de reportes diarios, y solucion de ciertos errores
    tambien se implemento el cambio de funcinalidades de admin/soporte

--- GUI/sub_ventanas/inventario_productos.py
+++ GUI/sub_ventanas/inventario_productos.py
@@ -3,41 +3,10 @@ from PyQt5.uic import loadUi
 from PyQt5.QtWidgets import QMainWindow, QHeaderView, QTableWidgetItem, QCompleter
 from PyQt5.QtCore import QPropertyAnimation, Qt, QStringListModel
 from PyQt5 import QtCore, QtGui
-from PyQt5.QtGui import QPainter, QBrush, QColor
 from API.DATA import GestionDatos
 from API.prueba import Inventario
 
-class CBackground:
-    def paintEvent(self, event):
-        painter = QPainter(self)
-        painter.setPen(Qt.NoPen)
-        painter.setRenderHint(QPainter.Antialiasing)
-
-        brocha1 = QBrush(QColor(212, 132, 180), Qt.SolidPattern)
-        brocha2 = QBrush(QColor(228, 156, 198), Qt.SolidPattern)
-        brocha3 = QBrush(QColor(235, 188, 220), Qt.SolidPattern)
-
-        # Dibujando circulos abajo
-        painter.setBrush(brocha3)
-        painter.drawEllipse(140, 530, 200, 200)
-
-        painter.setBrush(brocha2)
-        painter.drawEllipse(40, 480, 200, 200)
-
-        painter.setBrush(brocha1)
-        painter.drawEllipse(-70, 440, 200, 200)
-
-        # Dibujando circulos arriba
-        painter.setBrush(brocha3)
-        painter.drawEllipse(440, -140, 200, 200)
-
-        painter.setBrush(brocha2)
-        painter.drawEllipse(550, -100, 200, 200)
-
-        painter.setBrush(brocha1)
-        painter.drawEllipse(700, -70, 200, 200)
-
-        painter.end()
+from GUI.sub_ventanas.utils.css import CBackground
 
 class InventarioProductos(QMainWindow, CBackground):
     def __init__(self):
